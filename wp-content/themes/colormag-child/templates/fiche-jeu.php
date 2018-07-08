d6632e90cf18399216ba4f74c976725d09818784
Thomas Di Mascio <retroadmin@gamesmarket.fr>
2018-07-08 15:30:41 +0200

    Ajout Lien vers differente region + console
    Ajout d'un lien qui redirige vers le même jeu sur une région et plate-forme differente

--- wp-content/themes/colormag-child/templates/fiche-jeu.php
+++ wp-content/themes/colormag-child/templates/fiche-jeu.php
@@ -1,293 +1,405 @@
-<?php
+				<?php
 
-	/*
-	Template Name: Fiche Jeu
-	*/
+					/*
+					Template Name: Fiche Jeu
+					*/
 
-  // Init
-    get_header();
+				  // Init
+					get_header();
 
-  // Recuperation slug
-    $slug = $wp_query->query_vars['slug'];
+				  // Recuperation slug
+					$slug = $wp_query->query_vars['slug'];
 
-  // Get Data Game
-    global $wpdb;
-    $result = $wpdb->get_results( "SELECT * FROM Master_DB_Games WHERE slug='".$slug."' ");
-    $game = $result[0];
-    $console = $wpdb->get_results("SELECT * FROM Master_DB_Consoles");
-    //echo '<pre>';
-    //var_dump($console);
-    //echo '</pre>';
-
-?>
-
-<style type="text/css">
-    body{
-    	font-size: 10px;
-    }
-    .row{
-    	position: relative;
-    	width: 50%;
-    }
-
-    .package{
-    	border-radius: 12px;
-    	box-shadow: 0px 0px 11px #289dcc;
-    }
-    .packages .package {
-    	background: #fff;
-    	margin-top: 25px;
-    	margin-bottom: 20px;
-    	padding-bottom: 5px;
-    	text-align: center;
-    	border: solid 1px #38a7bb;
-    	overflow: hidden;
-    }
-    .packages .package .package-header {
-    	height: 50px;
-    	background: #289dcc;
-    }
-
-    .packages .package .package-header h5 {
-    	color: #fff;
-    	text-transform: uppercase;
-    	font-weight: bold;
-    	line-height: 47px;
-    	margin: 0;
-    	letter-spacing: 0.08em;
-    }
-    .packages .package .package-header.light-gray h5 {
-    	color: white;
-    }
-    .packages .package .price {
-    	height: 5%;
-    	color: #fff;
-    }
-    .packages .package ul {
-    	padding: 0;
-    	font-size: 20px;
-    }
-    .packages .package ul li {
-    	list-style-type: none;
-    	padding-top: 10px;
-    	padding-bottom: 10px;
-    	width: 90%;
-    	margin: auto;
-    	border-bottom: 1px dotted #ccc;
-    }
-    .packages .package ul li:last-child {
-    	border-bottom: 0;
-    }
-    .title_tab{
-    	font-size: 18px;
-    }
-    #global_id{
-    	border-width:1px 1px 1px 1px;
-    	border-style:solid;
-    	border-color: black;
-    	border-radius: 12px;
-    	background-color: #F5F5F5;
-    	box-shadow: 0px 0px 11px #000000;
-    	margin:0 auto;
-    	width: 900px;
-    	height: 375px;
-    	margin-left: -50px;
-    }
-    #global_id .gauche{
-    	float: left;
-    	margin-top: 25px;
-    	margin-right: 20px;
-    	margin-left: 20px;
-    	margin-bottom: 25px;
-    }
-    #global_id .droite{
-    	float: left;
-    	width: 300px;
-    }
-    #global_id .ex-droite{
-    	float: left;
-    	width: 300px;
-    	margin-left: 20px;
-    }
-
-    .market {
-    	margin:0 auto;
-    	width: 1000px;
-    	margin-top: 25px;
-    	margin-bottom: 25px;
-    	margin-left: -100px;
-    }
-    .market_tab{
-    	border:0;
-    }
-
-    thead tr th{
-    	font-family: "Gadugi";
-    	text-align: center;
-    		font-size: 13px;
-    		background-color: #289dcc;
-    		color: white;
-    }
-
-    .ligne_tab td{
-    	font-family: "Gadugi";
-    		text-align: center;
-    		font-size: 11px;
-    }
-
-    .donnees{
-    	font-size: 15px;
-    }
-
-    .gauche img{
-    	border-radius: 12px;
-    	box-shadow: 0px 0px 11px #289dcc;
-    }
-
-</style>
+				  // Get Data Game
+					global $wpdb;
+					$result = $wpdb->get_results( "SELECT * FROM Master_DB_Games WHERE slug='".$slug."' ");
+					$game = $result[0];
+					$console = $wpdb->get_results("SELECT * FROM Master_DB_Consoles");
+					$consoles = $console[0];
+					$constructeur = $wpdb->get_results("SELECT * FROM Master_DB_Constructeurs");
+					$doublons = "SELECT * FROM Master_DB_Games";
+					$requete = $wpdb->get_results($doublons);
 
 
+					?>
 
+					<style type="text/css">
+					body{
+						font-size: 10px;
+					}
+					.row{
+						position: relative;
+						width: 50%;
+					}
 
+					.package{
+						border-radius: 12px;
+						box-shadow: 0px 0px 11px #289dcc;
+					}
+					.packages .package {
+						background: #fff;
+						margin-top: 25px;
+						margin-bottom: 20px;
+						padding-bottom: 5px;
+						text-align: center;
+						border: solid 1px #38a7bb;
+						overflow: hidden;
+					}
+					.packages .package .package-header {
+						height: 50px;
+						background: #289dcc;
+					}
 
+					.packages .package .package-header h5 {
+						color: #fff;
+						text-transform: uppercase;
+						font-weight: bold;
+						line-height: 47px;
+						margin: 0;
+						letter-spacing: 0.08em;
+					}
+					.packages .package .package-header.light-gray h5 {
+						color: white;
+					}
+					.packages .package .price {
+						height: 5%;
+						color: #fff;
+					}
+					.packages .package ul {
+						padding: 0;
+						font-size: 20px;
+					}
+					.packages .package ul li {
+						list-style-type: none;
+						padding-top: 10px;
+						padding-bottom: 10px;
+						width: 90%;
+						margin: auto;
+						border-bottom: 1px dotted #ccc;
+					}
+					.packages .package ul li:last-child {
+						border-bottom: 0;
+					}
+					.title_tab{
+						font-size: 18px;
+					}
+					#global_id{
+						border-width:1px 1px 1px 1px;
+						border-style:solid;
+						border-color: black;
+						border-radius: 12px;
+						background-color: #F5F5F5;
+						box-shadow: 0px 0px 11px #000000;
+						margin:0 auto;
+						width: 900px;
+						height: 375px;
+						margin-left: -50px;
+					}
+					#global_id .gauche{
+						float: left;
+						margin-top: 25px;
+						margin-right: 20px;
+						margin-left: 20px;
+						margin-bottom: 25px;
+					}
+					#global_id .droite{
+						float: left;
+						width: 300px;
+					}
+					#global_id .ex-droite{
+						float: left;
+						width: 300px;
+						margin-left: 20px;
+					}
 
-<style media="screen">
-  .ficheJeu{
-    text-align: center;
-  }
-  .ficheJeu .tableData{
-    width:100%;
-    max-width: 900px;
-    margin:0 auto;
-    overflow: hidden;
-    padding:20px 30px 5px;
-    border-radius:25px;
-    border: solid 1px #000;
-    box-shadow: 0px 0px 12px #DC27FC;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-  .ficheJeu .tableData .column{
-    float: left;
-    width:30%;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-  .ficheJeu .tableData .middle{
-    margin:0 5%;
-  }
-  .ficheJeu .tableData .column img{
-    max-width:100%;
-    max-height:325px;
-    border-radius: 12px;
-    border: solid 1px #DC27FC;
-    box-shadow: 0px 0px 11px #DC27FC;
-  }
-  .ficheJeu .tableData .column table{
-    background: #fff;
-    text-align: center;
-    border: solid 1px #DC27FC;
-    overflow: hidden;
-    border-radius: 12px;
-    box-shadow: 0px 0px 11px #DC27FC;
-  }
-  .ficheJeu .tableData .column table tr{
-  }
-  .ficheJeu .tableData .column table tr th{
-    height: 50px;
-    margin: 0;
-    background: #000;
-    color: white;
-    font-size: 20px;
-    line-height: 47px;
-    font-weight: bold;
-    letter-spacing: 0.08em;
-    text-transform: uppercase;
-    text-align: center;
-  }
-  .ficheJeu .tableData .column table tr td{
-    font-size: 16px;
-    line-height: 37px;
-  }
-  .ficheJeu .market{
-    width:100%;
-    margin:0;
-    margin-top:35px;
-  }
-  .ficheJeu .market table tr th{
-    background:#000;
-    color:#fff;
-  }
-</style>
+					.market {
+						margin:0 auto;
+						width: 1000px;
+						margin-top: 25px;
+						margin-bottom: 25px;
+						margin-left: -100px;
+					}
+					.market_tab{
+						border:0;
+					}
+
+					thead tr th{
+						font-family: "Gadugi";
+						text-align: center;
+						font-size: 13px;
+						background-color: #289dcc;
+						color: white;
+					}
+
+					.ligne_tab td{
+						font-family: "Gadugi";
+						text-align: center;
+						font-size: 11px;
+					}
+
+					.donnees{
+						font-size: 15px;
+					}
+
+					.gauche img{
+						border-radius: 12px;
+						box-shadow: 0px 0px 11px #289dcc;
+					}
+
+				</style>
 
 
 
 
 
 
-<section class="ficheJeu">
+				<style media="screen">
+				.ficheJeu{
+					text-align: center;
+				}
+				.ficheJeu .tableData{
+					width:85%;
+					margin:0 auto;
+					overflow: hidden;
+					border-radius:25px;
+				}
+				.ficheJeu .tableData .column{
+					float: left;
+					width:30%;
+					-webkit-box-sizing: border-box;
+					box-sizing: border-box;
+					min-height: 40vh;
+				}
+				.left{
+					width: 20%!important; 
+				}
+				.ficheJeu .tableData .middle{
+					margin:0 5%;
+				}
+				.ficheJeu .tableData .column img{
+					max-width:100%;
+					max-height:280px;
+					border-radius: 12px;
 
-	<header class="entry-header">
-		<h1 class="entry-title"><?php echo $game->NOM; ?></h1>
-	</header>
-
-  <div class="tableData">
-    <div class="column">
-      <?php echo '<img class="cover" src="'.$game->IMAGE.'" alt="Cover du jeu '.$game->NOM.'"/>'; ?>
-    </div>
-    <div class="column middle">
-      <table>
-        <tr><th>Fiche</th></tr>
-        <tr><td><b>Console :</b> <?php echo $console[$game->CONSOLE]->NOM?></td></tr>
-        <tr><td><b>Date de sortie :</b> <?php echo $game->DATE; ?></td></tr>
-        <tr><td><b>Genre :</b> <?php echo $game->GENRE; ?></td></tr>
-        <tr><td><b>Editeur :</b> <?php echo $game->EDITEUR; ?></td></tr>
-        <tr><td><b>Développeur :</b> <?php echo $game->DEVELOPPEUR; ?></td></tr>
-      </table>
-    </div>
-    <div class="column">
-      <table>
-        <tr><th>Statistiques</th></tr>
-        <tr><td><b>Prix moyen en loose :</b></td></tr>
-        <tr><td><b>Prix moyen en complet :</b></td></tr>
-        <tr><td><b>CCC :</b></td></tr>
-        <tr><td><b>DDD :</b></td></tr>
-        <tr><td><b>EEE :</b></td></tr>
-      </table>
-    </div>
-  </div>
-
-  <div class="market">
-    <table>
-      <tr>
-        <th>Vendeurs</th>
-        <th>Etat</th>
-        <th>Boite</th>
-        <th>Notice</th>
-        <th>Jeu</th>
-        <th>Prix</th>
-        <th>Commentaires</th>
-        <th>Acheter</th>
-      </tr>
-      <tr>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>
-    </table>
-  </div>
-
-</section>
+				}
+				.ficheJeu .tableData .column table{
+					background: #fff;
+					text-align: center;
+					border: solid 2px #00000000;
+					overflow: hidden;
+					border-radius: 12px;
+				}
+				.ficheJeu .tableData .column table tr{
+				}
+				.ficheJeu .tableData .column table tr th{
+					height: 50px;
+					margin: 0;
+					background: #000;
+					color: white;
+					font-size: 20px;
+					line-height: 47px;
+					font-weight: bold;
+					letter-spacing: 0.08em;
+					text-transform: uppercase;
+					text-align: center;
+				}
+				.ficheJeu .tableData .column table tr td{
+					font-size: 16px;
+				}
+				.ficheJeu .market{
+					width:100%;
+					margin:0;
+					margin-top:35px;
+				}
+				.ficheJeu .market table tr th{
+					background:#000;
+					color:#fff;
+				}
+			</style>
 
 
 
 
 
 
+			<section class="ficheJeu">
 
-<?php get_footer(); ?>
+				<header class="entry-header">
+					<h1 class="entry-title" style="font-family: 'Orbitron'"><?php echo $game->NOM; ?></h1>
+				</header>
+
+				<div class="tableData">
+					<div class="column left">
+						<?php echo '<img class="cover" src="'.$game->IMAGE.'" alt="Cover du jeu '.$game->NOM.'"/>';
+						?> 
+						<div>
+							<p> 
+								<?php $constructeur_name = $console[$game->CONSOLE]->CONSTRUCTEUR;
+								$i = 0; 
+								foreach ($requete as $key => $value) {
+									if($i == 0){
+										?>
+										<style type="text/css">
+										#region_check{
+											display: none;
+										}
+										</style><?php 
+										$i++;
+									}
+									elseif(($game->NOM == $value->NOM) && ($game->REGION != $value->REGION) && ($game->CONSOLE == $value->CONSOLE)){
+										?>
+										<style type="text/css">
+										#region_check{
+											display: block;
+											text-align: left;
+										}
+										</style><?php
+										if($i == 1){
+											echo '<span id="region_check">Disponible en : ';
+											echo '<a href="'.get_site_url().'/'.$constructeur[$constructeur_name]->SLUG.'/'.$console[$value->CONSOLE]->SLUG.'/'.$value->REGION.'/'.$value->SLUG.'">'.$value->REGION.'</a> ';
+											$i++;
+										}
+										else{
+											echo "| ";
+											echo '<a href="'.get_site_url().'/'.$constructeur[$constructeur_name]->SLUG.'/'.$console[$value->CONSOLE]->SLUG.'/'.$value->REGION.'/'.$value->SLUG.'">'.$value->REGION.'</a> </span>';
+										}
+									}
+								}
+								;?></p>
+								<p><?php
+								$i = 0;
+								foreach ($requete as $key => $value) {
+									$constructeur_name = $console[$value->CONSOLE]->CONSTRUCTEUR;	
+									if($i == 0){
+										?>
+										<style type="text/css">
+										#console_check{
+											display: none;
+										}
+										</style><?php 
+										$i++;
+									}
+									elseif(($game->NOM == $value->NOM) && ($game->CONSOLE != $value->CONSOLE) && ($game->REGION == $value->REGION)){
+										?>
+										<style type="text/css">
+										#console_check{
+											display: block;
+											text-align: left;
+										}
+										</style><?php
+										if($i == 1){
+											echo '<span id="console_check">Disponible sur : ';
+											echo '<a href="'.get_site_url().'/'.$constructeur[$constructeur_name]->SLUG.'/'.$console[$value->CONSOLE]->SLUG.'/'.$value->REGION.'/'.$value->SLUG.'">'.$console[$value->CONSOLE]->NOM.'</a> ';
+											$i++;
+										}
+										else{
+											echo "| ";
+											echo '<a href="'.get_site_url().'/'.$constructeur[$constructeur_name]->SLUG.'/'.$console[$value->CONSOLE]->SLUG.'/'.$value->REGION.'/'.$value->SLUG.'">'.$console[$value->CONSOLE]->NOM.'</a> </span>';
+										}
+									}}
+									;?></p>
+								</div>
+							</div>
+
+							<div class="column middle">
+								<table>
+									<tr><th>Fiche</th></tr>
+									<tr><td><b>Console :</b> <?php $constructeur_name = $console[$game->CONSOLE]->CONSTRUCTEUR; echo $constructeur[$constructeur_name]->NOM;echo " "; echo $console[$game->CONSOLE]->NOM?></td></tr>
+									<tr><td><b>Region :</b> <?php 
+									if ($game->REGION == 'JAP'){
+										echo 'Japon';
+									}
+									elseif ($game->REGION == 'PAL') {
+										echo 'Europe';
+									}
+									elseif ($game->REGION == 'US') {
+										echo 'Amérique';
+									}
+									elseif ($game->REGION == 'AUS') {
+										echo 'Australie';
+									}
+									elseif ($game->REGION == 'CAN') {
+										echo 'Canada';
+									}
+									elseif ($game->REGION == 'MEX') {
+										echo 'Mexique';
+									}
+									elseif ($game->REGION == 'CHN') {
+										echo 'Chine';
+									}
+									elseif ($game->REGION == 'KOR') {
+										echo 'Corée du Sud';
+									}
+									elseif ($game->REGION == 'TWN') {
+										echo 'Taïwan';
+									}
+									elseif ($game->REGION == 'MAL') {
+										echo 'Malaisie';
+									}
+									elseif ($game->REGION == 'BRA') {
+										echo 'Brésil';
+									}
+									elseif ($game->REGION == 'ARG') {
+										echo 'Argentine';
+									};?>
+									<tr><td><b>Date de sortie :</b> <?php
+									$day_month = '01/01';
+									if  (stripos($game->DATE, $day_month) === FALSE){
+										echo $game->DATE;}
+										else{
+											echo substr($game->DATE, 6);
+										} ?></td></tr>
+										<?php
+										if ($game->TYPE == 'jeu'){
+											echo '<tr><td><b>Genre :</b> '.$game->GENRE.'</td></tr>';
+											echo '<tr><td><b>Editeur :</b> '.$game->EDITEUR.'</td></tr>';
+											echo '<tr><td><b>Developpeur :</b> '.$game->DEVELOPPEUR.'</td></tr>';}?>
+										</table>
+									</div>
+									<div class="column">
+										<table>
+											<tr><th>Statistiques</th></tr>
+											<tr><td><b>Prix moyen en loose :</b></td></tr>
+											<tr><td><b>Prix moyen en complet :</b></td></tr>
+											<tr><td><b>CCC :</b></td></tr>
+											<tr><td><b>DDD :</b></td></tr>
+											<tr><td><b>EEE :</b></td></tr>
+										</table>
+									</div>
+
+								</div>
+
+								<div class="market">
+									<table>
+										<tr>
+											<th>Vendeurs</th>
+											<th>Etat</th>
+											<th>Boite</th>
+											<th>Notice</th>
+											<th>Jeu</th>
+											<th>Prix</th>
+											<th>Commentaires</th>
+											<th>Acheter</th>
+										</tr>
+										<tr>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+											<td>-</td>
+										</tr>
+									</table>
+								</div>
+
+							</section>
+
+
+
+
+
+
+
+							<?php get_footer(); ?>
--- wp-content/themes/colormag-child/templates/page-consoles.php
+++ wp-content/themes/colormag-child/templates/page-consoles.php
@@ -1,72 +1,78 @@
-	<?php
-		/*
-		Template Name: Page Console
-		*/
+		<?php
+			/*
+			Template Name: Page Console
+			*/
 
-	  // Init
-		get_header();
+		  // Init
+			get_header();
 
-	  // Recuperation slug
-		$slug = $wp_query->query_vars['slug'];
+		  // Recuperation slug
+			$slug = $wp_query->query_vars['slug'];
 
-	  // Get Data Game
-		global $wpdb;
-		$console = $wpdb->get_results("SELECT SLUG, IMAGE, CONSTRUCTEUR, LOGO FROM Master_DB_Consoles WHERE slug='".$slug."'");
-		$games = $wpdb->get_results("SELECT G.NOM, G.IMAGE, G.GENRE, G.SLUG, G.REGION, G.TYPE FROM Master_DB_Games G, Master_DB_Consoles C WHERE C.slug='".$slug."' AND C.ID = G.CONSOLE ORDER BY G.NOM");
-		$packs = $wpdb->get_results("SELECT P.NOM, P.IMAGE, P.SLUG, P.REGION, P.TYPE FROM Master_DB_Packs P, Master_DB_Consoles C WHERE C.slug='".$slug."' AND C.ID = P.CONSOLE ORDER BY P.NOM");
-		$constructeur = $wpdb->get_results("SELECT SLUG FROM Master_DB_Constructeurs WHERE ID='".$console[0]->CONSTRUCTEUR."'");
-		$results = array_merge($games, $packs);
-	    // echo '<pre>';
-	    // // var_dump($slug);
-	    // // var_dump($console);
-	    // // var_dump($games);
-	    // // var_dump($constructeur);
-	    // echo '</pre>';
+		  // Get Data Game
+			global $wpdb;
+			$console = $wpdb->get_results("SELECT SLUG, IMAGE, CONSTRUCTEUR, LOGO FROM Master_DB_Consoles WHERE slug='".$slug."'");
+			$games = $wpdb->get_results("SELECT G.NOM, G.IMAGE, G.GENRE, G.SLUG, G.REGION, G.TYPE FROM Master_DB_Games G, Master_DB_Consoles C WHERE C.slug='".$slug."' AND C.ID = G.CONSOLE ORDER BY G.NOM");
+			$packs = $wpdb->get_results("SELECT P.NOM, P.IMAGE, P.SLUG, P.REGION, P.TYPE FROM Master_DB_Packs P, Master_DB_Consoles C WHERE C.slug='".$slug."' AND C.ID = P.CONSOLE ORDER BY P.NOM");
+			$constructeur = $wpdb->get_results("SELECT SLUG FROM Master_DB_Constructeurs WHERE ID='".$console[0]->CONSTRUCTEUR."'");
+			$results = array_merge($games, $packs);
+		    // echo '<pre>';
+		    // // var_dump($slug);
+		    // // var_dump($console);
+		    // // var_dump($games);
+		    // // var_dump($constructeur);
+		    // echo '</pre>';
 
 
-		?>
-		<style type="text/css">
-		.light{
-			color: yellow;
-		}
-	</style>
-	<div class="wrap" id="pageConsole">
-		<img class="mainLogo" src="http://gamesmarket.fr/img<?php echo $console[0]->LOGO; ?>" alt="Logo du constructeur">
-		<div><?php
-		echo '<form method="GET" action="'.get_site_url().'/'.$constructeur[0]->SLUG.'/'.$slug.'""></form>'?>
-		<span>
-			<label>Type</label><br>
-			<span><select class="filter" data-filter="console" autocomplete="off" ">
-				<option value="all" selected="selected">Tous</option>
-				<option value="consoles">Console</option>
-				<option value="jeu">Jeu</option>
-			</select></span>
-			<label>Rechercher</label>
-			<input type="text" name="search" id="categoryfilter" placeholder="chercher le jeu">
-		</span>
-	</div>
-	<table id="tableid">
-		<tbody>
-			<thead>
-				<th>Photo</th>
-				<th>Type</th>
-				<th>Nom</th>
-				<th>Region</th>
-				<th>Genre</th>
-				<th>Disponibe</th>
-				<th>A partir de</th>
-			</thead>
-			<?php
-			foreach ($results as $key => $value) {
-				if ($value->TYPE == "jeu"){
-					echo '<tr data-console="jeu">';
-					echo '<td>'.do_shortcode('[icon name="camera-retro" class="" unprefixed_class=""]').'</td>';
-					echo '<td id="type_jeu">Jeu</td>';}
-					else{
+			?>
+			<style type="text/css">
+			.light{
+				color: yellow;
+			}
+		</style>
+		<div class="wrap" id="pageConsole">
+			<img class="mainLogo" src="http://gamesmarket.fr/img<?php echo $console[0]->LOGO; ?>" alt="Logo du constructeur">
+			<div><?php
+			echo '<form method="GET" action="'.get_site_url().'/'.$constructeur[0]->SLUG.'/'.$slug.'""></form>'?>
+			<span>
+				<label>Type</label><br>
+				<span><select class="filter" data-filter="console" autocomplete="off" ">
+					<option value="all" selected="selected">Tous</option>
+					<option value="consoles">Console</option>
+					<option value="jeu">Jeu</option>
+				</select></span>
+				<label>Rechercher</label>
+				<input type="text" name="search" id="categoryfilter" placeholder="chercher le jeu">
+			</span>
+		</div>
+		<table id="tableid">
+			<tbody>
+				<thead>
+					<th>Photo</th>
+					<th>Type</th>
+					<th>Nom</th>
+					<th>Region</th>
+					<th>Genre</th>
+					<th>Disponibe</th>
+					<th>A partir de</th>
+				</thead>
+				<?php
+				foreach ($results as $key => $value) {
+					if (isset($value->TYPE) && $value->TYPE == "jeu"){
+						echo '<tr data-console="jeu">';
+						echo '<td>'.do_shortcode('[icon name="camera-retro" class="" unprefixed_class=""]').'</td>';
+						echo '<td id="type_jeu">Jeu</td>';
+					}
+					elseif (isset($value->TYPE) && $value->TYPE == "console"){
 						echo '<tr data-console="consoles">';
 						echo '<td>'.do_shortcode('[icon name="camera-retro" class="" unprefixed_class=""]').'</td>';
 						echo '<td>Console</td>';
-					};
+					}
+					else{
+						echo '<tr data-console="">';
+						echo '<td>'.do_shortcode('[icon name="camera-retro" class="" unprefixed_class=""]').'</td>';
+						echo '<td>-</td>';
+					}
 					echo '<td><a id="filtre_game_name" href="'.get_site_url().'/'.$constructeur[0]->SLUG.'/'.$console[0]->SLUG.'/'.$value->REGION.'/'.$value->SLUG.'" title="'.$value->NOM.'"><span>'.$value->NOM.'</span></a></td>';
 					if ($value->REGION == 'JAP'){
 						echo '<td><img src="/img/flags/japan_flag.png" alt="japan_flag" width="20px" style="margin-bottom:0;"/></td>';
@@ -79,6 +85,30 @@
 					}
 					elseif ($value->REGION == 'AUS') {
 						echo '<td><img src="/img/flags/aus_flag.png" alt="aus_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'CAN') {
+						echo '<td><img src="/img/flags/can_flag.png" alt="can_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'MEX') {
+						echo '<td><img src="/img/flags/mex_flag.png" alt="mex_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'CHN') {
+						echo '<td><img src="/img/flags/chn_flag.png" alt="chn_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'KOR') {
+						echo '<td><img src="/img/flags/kor_flag.png" alt="kor_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'TWN') {
+						echo '<td><img src="/img/flags/twn_flag.png" alt="twn_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'MAL') {
+						echo '<td><img src="/img/flags/mal_flag.png" alt="mal_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'BRA') {
+						echo '<td><img src="/img/flags/bra_flag.png" alt="bra_flag" width="20px" style="margin-bottom:0;"/></td>';
+					}
+					elseif ($value->REGION == 'ARG') {
+						echo '<td><img src="/img/flags/arg_flag.png" alt="arg_flag" width="20px" style="margin-bottom:0;"/></td>';
 					};
 					if (isset($value->GENRE)){
 						echo '<td>'.$value->GENRE.'</td>';}
@@ -117,52 +147,52 @@
 					});
 				});
 				jQuery('.filter').change(function(){
-	    // on cache toutes les lignes
-	    jQuery('tbody tr').hide();      
-	    // on parcourt les différents filtres.
-	    var dataSelector = ""; // variable qui servira à stocker les filtres et leur valeur
-	    jQuery('.filter').each(function(){
-	        // on récupère le data-filter et la value du select
-	        var filter = jQuery(this).data('filter'),
-	            valeur = jQuery(this).val();
-	        // si le filtre est utilisé on stocke la valeur
-	        if(valeur != 0 && valeur != 'all'){      
-		            dataSelector += "[data-"+filter+"="+valeur+"]";
-	        } 
-    });
-	      
-	    // on affiche toutes les lignes correspondant aux filtre.
-	jQuery('thead').show();
-	    jQuery('tbody tr'+dataSelector).show();
-});
-	//Dynamic_Search
-	(function($){
-		jQuery('#categoryfilter').keyup(function(event){
-			var input = jQuery(this);
-			var val = input.val();
-			var regexp = '\\b(.*)';
-			for(var i in val){
-				regexp += '('+val[i]+')';
-			}
-			regexp += '(.*)\\b';
-			$('span').parent().parent().parent().show()
-			$('td').find('a>span').each(function(){
-				var span = $(this);
-				var resultats = span.text().match(new RegExp(regexp,'i'));
-				if (resultats){
-					var string ='';
-					for(var i in resultats){
-						if(i > 0){
-							string += resultats[i];
+		    // on cache toutes les lignes
+		    jQuery('tbody tr').hide();      
+		    // on parcourt les différents filtres.
+		    var dataSelector = ""; // variable qui servira à stocker les filtres et leur valeur
+		    jQuery('.filter').each(function(){
+		        // on récupère le data-filter et la value du select
+		        var filter = jQuery(this).data('filter'),
+		            valeur = jQuery(this).val();
+		        // si le filtre est utilisé on stocke la valeur
+		        if(valeur != 0 && valeur != 'all'){      
+			            dataSelector += "[data-"+filter+"="+valeur+"]";
+		        } 
+	    });
+		      
+		    // on affiche toutes les lignes correspondant aux filtre.
+		jQuery('thead').show();
+		    jQuery('tbody tr'+dataSelector).show();
+	});
+		//Dynamic_Search
+		(function($){
+			jQuery('#categoryfilter').keyup(function(event){
+				var input = jQuery(this);
+				var val = input.val();
+				var regexp = '\\b(.*)';
+				for(var i in val){
+					regexp += '('+val[i]+')';
+				}
+				regexp += '(.*)\\b';
+				$('span').parent().parent().parent().show()
+				$('td').find('a>span').each(function(){
+					var span = $(this);
+					var resultats = span.text().match(new RegExp(regexp,'i'));
+					if (resultats){
+						var string ='';
+						for(var i in resultats){
+							if(i > 0){
+								string += resultats[i];
+							}
 						}
+						span.empty().append(string);
 					}
-					span.empty().append(string);
-				}
-				else{
-					span.parent().parent().parent().hide();
-				}
-			})
-		});
-	}) (jQuery);
+					else{
+						span.parent().parent().parent().hide();
+					}
+				})
+			});
+		}) (jQuery);
 
-</script>
+	</script>
